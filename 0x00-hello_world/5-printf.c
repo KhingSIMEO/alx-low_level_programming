d6ee6a4942f50c2284ea42e06bd497c67520a27a
KhingSIMEO <Kingsimeo@gmail.com>
2023-02-16 14:11:34 -0800

    5 Printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints a new line with printf function
+ * Return: 0 (WellDone)
+ */
+int main(void)
+{
+	Printf("with proper grammar\, but the outcome is a piece of art\,");
+	return (0);
+}
