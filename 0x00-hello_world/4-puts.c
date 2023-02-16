2e9f7e746a7110531fc78aa54cd9c237cac46cb0
KhingSIMEO <Kingsimeo@gmail.com>
2023-02-16 13:53:00 -0800

    4 Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C Program that a new line with put function without printf, and ends with value 0
+ * Return: 0 (Well Done Boy)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
