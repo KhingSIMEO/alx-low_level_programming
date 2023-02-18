ff0cecd52f670ab210c5607be54dfe966a32e296
KhingSIMEO <Kingsimeo@gmail.com>
2023-02-18 03:29:01 -0800

    reverse Alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry - point
+ *
+ * Return: Always 0 (WelDONE)
+ */
+
+int main(void)
+{
+	char v;
+
+
+	v = 'z';
+	while
+		(v >= 'a') {
+			putchar(v);
+			v--;
+		}
+	putchar('\n');
+	return (0);
+}
