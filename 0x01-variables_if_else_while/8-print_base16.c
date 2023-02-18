f9d752e56db0e55478799c817ba0796460564439
KhingSIMEO <Kingsimeo@gmail.com>
2023-02-18 03:58:59 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry - Point
+ *
+ * Return: 0 (WelDONE)
+ */
+
+int main(void)
+{
+	char c;
+
+	int b;
+
+	c = 'a';
+	while
+		(b < 10) {
+			putchar(b + '0');
+			b++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
