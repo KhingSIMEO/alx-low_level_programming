9687a96499643f97e9a576fe38a3ed44f92f3fc7
KhingSIMEO <Kingsimo@gmail.com>
2023-03-31 03:52:31 -0700

    Changed lists.h content

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-/**
+void first(void) __attribute__ ((constructor));
+
+/*
  * first - prints a sentence before the main
+ *
  * function is executed
- * Return: Always 0
  */
+
 void first(void)
 {
 printf("You're beat! and yet, you must allow,\n");
