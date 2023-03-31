9adaca1db86dc11ebb96933f61b49fc419ef1285
KhingSIMEO <Kingsimo@gmail.com>
2023-03-31 03:31:05 -0700

    Update String

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * first - prints a sentence before the main
  * function is executed
+ * Return: Always 0
  */
 void first(void)
 {
